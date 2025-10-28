a5582a86375fad4d29439fc1c28df357cda5bf3b
Ayla-van-de-vosse <ayla@vandevosse.net>
2025-10-28 13:45:29 +0100

    Add ReversiGame class with basic structure

---
+++ src/nl/isy_games/ReversiGame.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class ReversiGame extends BoardGame {
+    public ReversiGame() {
+        initializeBoard(8, 8);
+    }
+
+    // placeholder methods
+    public void makeMove(int row, int col) { }
+    public void checkGameOver() { }
+    public void aiMove() { }
+}
