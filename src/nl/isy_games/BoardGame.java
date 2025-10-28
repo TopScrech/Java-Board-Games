20f6fcb21cc30fbaf33ede4cae93f450b81241a3
Ayla-van-de-vosse <ayla@vandevosse.net>
2025-10-28 13:47:22 +0100

    Refactor BoardGame class to extend JPanel and add methods

--- src/nl/isy_games/BoardGame.java
+++ src/nl/isy_games/BoardGame.java
@@ -1,13 +1,35 @@
-package nl.isy_games;
+package classes;
 
-public abstract class BoardGame {
-    public abstract boolean makeMove(Player player, int row, int col);
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
 
-    public abstract boolean isGameOver();
+public abstract class BoardGame extends JPanel {
+    protected int rows;
+    protected int cols;
+    protected String[][] board;
 
-    public abstract Player getWinner();
+    public void initializeBoard(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        board = new String[rows][cols];
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                board[r][c] = "";
+    }
 
-    public abstract void printBoard();
-
-//    public abstract void printBoard();
+    public boolean isCellEmpty(int r, int c) { return board[r][c].isEmpty(); }
+    public void setCell(int r, int c, String symbol) { board[r][c] = symbol; }
+    public String[][] getBoardState() {
+        String[][] copy = new String[rows][cols];
+        for (int r=0; r<rows; r++)
+            System.arraycopy(board[r], 0, copy[r], 0, cols);
+        return copy;
+    }
+    public boolean isFull() {
+        for (int r=0; r<rows; r++)
+            for (int c=0; c<cols; c++)
+                if (board[r][c].isEmpty()) return false;
+        return true;
+    }
 }
