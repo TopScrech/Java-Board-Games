52af92bca04c07f0dff12e9bdcaae1da02a70419
Ayla-van-de-vosse <ayla@vandevosse.net>
2025-10-29 16:34:29 +0100

    Refactor BoardGame to use constructor for initialization
    Refactor BoardGame class to use constructor for initialization and make fields final.

--- src/nl/isy_games/BoardGame.java
+++ src/nl/isy_games/BoardGame.java
@@ -1,15 +1,13 @@
 package classes;
 
 import javax.swing.*;
-import java.awt.*;
-import java.util.List;
 
 public abstract class BoardGame extends JPanel {
-    protected int rows;
-    protected int cols;
-    protected String[][] board;
+    protected final int rows;
+    protected final int cols;
+    protected final String[][] board;
 
-    public void initializeBoard(int rows, int cols) {
+    public BoardGame(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
         board = new String[rows][cols];
@@ -19,16 +17,19 @@ public abstract class BoardGame extends JPanel {
     }
 
     public boolean isCellEmpty(int r, int c) { return board[r][c].isEmpty(); }
+
     public void setCell(int r, int c, String symbol) { board[r][c] = symbol; }
+
     public String[][] getBoardState() {
         String[][] copy = new String[rows][cols];
-        for (int r=0; r<rows; r++)
+        for (int r = 0; r < rows; r++)
             System.arraycopy(board[r], 0, copy[r], 0, cols);
         return copy;
     }
+
     public boolean isFull() {
-        for (int r=0; r<rows; r++)
-            for (int c=0; c<cols; c++)
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
                 if (board[r][c].isEmpty()) return false;
         return true;
     }
